d5399ce7f87cf6fea27a81fa1fa12ab940a07e86
sebastiansiemianowski <rudph2@gmail.com>
2020-08-27 01:05:39 +0100

    Adds columns to Items table and updates test config

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # it { is_expected.to validate_presence_of(:product_code) }
-  # it { is_expected.to validate_presence_of(:name) }
-  # it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_presence_of(:product_code) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:price) }
 end
